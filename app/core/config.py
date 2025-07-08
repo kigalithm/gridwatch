d959939f5765ddbbf03a87aa1d45859e4382132b
izamha <glcode40@gmail.com>
2025-07-08 11:56:41 +0200

    twitter configs

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,12 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     CORS_ORIGINS: List[str] = ["*"]
 
+    BEARER_TOKEN: str
+    CONSUMER_KEY: str
+    CONSUMER_SECRET: str
+    ACCESS_TOKEN: str
+    ACCESS_TOKEN_SECRET: str
+
     TWITTER_API_LIMIT: int
     COOLDOWN_SECONDS: int
 
