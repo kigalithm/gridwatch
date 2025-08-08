3a55576fc70e27e4444c9d305172d129449b2c7c
izamha <glcode40@gmail.com>
2025-08-08 15:34:01 +0200

    water_issue_schema

--- app/wasac/schema/water_issue_schema.py
+++ app/wasac/schema/water_issue_schema.py
@@ -1,15 +1,35 @@
 from pydantic import BaseModel, Field
 from uuid import UUID
 from typing import Optional
-from enum import Enum
 from datetime import datetime
 from app.wasac.models.water_issue import IssueType
 from app.schema.common_schemas import LocationSchema
 
 
-class IssueCreate(BaseModel):
+class WaterIssueCreate(BaseModel):
     issue_type: Optional[IssueType] = Field(default=IssueType.WATER_CUT)
     description: Optional[str] = None
     phone_number: Optional[str] = None
     image_url: Optional[str] = None
     location: Optional[LocationSchema] = None
+
+
+class WaterIssueUpdate(BaseModel):
+    audio_description: str
+    location: LocationSchema
+    coordinates: list
+    affected_areas: list
+    affeccted_area_names: list
+
+
+class WaterIssueOut(WaterIssueCreate):
+    id: UUID
+    description: str
+    audio_description: str
+    is_resolved: bool
+    location: LocationSchema
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
