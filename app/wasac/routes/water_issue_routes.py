c81a4540dd8cc00a30e05ec61d62baaab246cec9
izamha <glcode40@gmail.com>
2025-08-08 17:17:40 +0200

    water_issue_routes

--- app/wasac/routes/water_issue_routes.py
+++ app/wasac/routes/water_issue_routes.py
@@ -19,3 +19,11 @@ water_issue_route = APIRouter(tags=["WaterIssue"])
 def create_water_issue(issue: WaterIssueCreate, db: Session = Depends(get_db)):
     """Create or update outage"""
     return crud_water.create_outage(issue, db)
+
+
+@water_issue_route.get("/all", response_model=list[WaterIssueOut])
+def read_all_water_issues(db: Session = Depends(get_db)):
+    issues = crud_water.get_all_issues(db)
+    if not issues:
+        raise HTTPException(status_code=404, detail="No issues found.")
+    return issues
