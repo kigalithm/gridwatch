6ad42799c6fdeff0bcc8e1e6fe8f8c6204a4d55c
izamha <glcode40@gmail.com>
2025-08-08 15:14:16 +0200

    water_issue

--- app/reg/models/location.py
+++ app/reg/models/location.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Float, Integer, Index, UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import JSONB
 from geoalchemy2 import Geometry
-from app.db.models.base_model import BaseModel
+from app.db.base_model import BaseModel
 
 
 class Location(BaseModel):
--- app/wasac/models/water_issue.py
+++ app/wasac/models/water_issue.py
@@ -1,10 +1,24 @@
 import uuid
 from sqlalchemy import Column, String, Float, Text, Enum, DateTime, Boolean
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.sql import func
-from enum import Enum as PyEnum
 from app.db.base_model import BaseModel
+from pgvector.sqlalchemy import Vector
+from enum import Enum as PyEnum
+
+
+class IssueType(str, PyEnum):
+    WATER_CUT = "water_cut"
+    BROKEN_PIPE = "broken_pipe"
+    NEW_COUNTER = "new_counter"
 
 
 class WaterIssue(BaseModel):
     __tablename__ = "water_issues"
+    description = Column(String, nullable=True)
+    audio_description = Column(String, nullable=True)
+    phone_number = Column(String(20), nullable=True)
+    embedding = Column(Vector(384), nullable=False)
+    image_url = Column(String, nullable=True)
+    is_resolved = Column(Boolean, default=False)
+    location = Column(String, nullable=True)
+    issue_type = Column(Enum(IssueType), nullable=False)
